22bb66cd4bd6c92970a525f6cf7eeba0327d93f6
Mohamed Bouhdifi <mohamed_bouhdifi@hotmail.com>
2023-03-29 12:01:11 +0200

    Popup fixed

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,39 +1,53 @@
 let subMenu =  document.getElementById("subMenu");
+const apiUrl = 'https://fortnite-api.com/v2/cosmetics/br';
+const container = document.querySelector('.skin-container');
 
 function toggleMenu(){
   subMenu.classList.toggle("open-menu");
 }
 
-const apiUrl = 'https://fortnite-api.com/v2/cosmetics/br';
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const skins = data.data.filter(item => item.type.value === 'outfit');
 
-async function getFortniteSkins() {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
+    for (let index = 0; index < 31; index++) {
+      const skin = skins[index];
 
-  const skins = data.data.filter(item => item.type.value === 'outfit');
+      if (skin.name != "null" && skin.images.featured != null) {
 
-  const container = document.querySelector('.container');
+        const card = document.createElement('div');
+        card.classList.add('skin-card');
+        card.innerHTML = `
+        <img src="${skin.images.icon}" alt="${skin.description}" data-src="${skin.images.featured}" data-name="${skin.name}">
+        <h3>${skin.name}</h3>
+      `;
+        container.appendChild(card);
+      }
 
-  for (let index = 0; index < 25; index++) {
-    const skin = skins[index];
-
-    if(skin.name != "null"){
-    const skinDiv = document.createElement('div');
-    skinDiv.classList.add('skin');
-
-    const skinImg = document.createElement('img');
-    skinImg.src = skin.images.icon;
-
-    const skinName = document.createElement('div');
-    skinName.classList.add('skin-name');
-    skinName.innerText = skin.name;
-
-    skinDiv.appendChild(skinImg);
-    skinDiv.appendChild(skinName);
-
-    container.appendChild(skinDiv);
     }
-  }
-}
 
-getFortniteSkins();
+  })
+  .catch(error => console.error(error));
+
+container.addEventListener('click', event => {
+  const skinCard = event.target.closest('.skin-card');
+  if (skinCard) {
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    popup.innerHTML = `
+        <div class="popup-content">
+          <button class="btn-close">&times;</button>
+          <img src="${skinCard.querySelector('img').dataset.src}" alt="${skinCard.querySelector('img').alt}">
+          <h1>${skinCard.querySelector('img').dataset.name}</h1>
+          <h3>${skinCard.querySelector('img').alt}</h3>
+        </div>
+      `;
+    document.body.appendChild(popup);
+
+    const closeBtn = popup.querySelector('.btn-close');
+    closeBtn.addEventListener('click', () => {
+      popup.remove();
+    });
+  }
+});
--- ww/script.js
+++ ww/script.js
@@ -5,36 +5,44 @@ fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     const skins = data.data.filter(item => item.type.value === 'outfit');
-    skins.forEach(skin => {
-      const card = document.createElement('div');
-      card.classList.add('skin-card');
-      card.innerHTML = `
-        <img src="${skin.images.icon}" alt="${skin.name}" data-src="${skin.images.featured}" data-name="${skin.name}">
+
+    for (let index = 0; index < 31; index++) {
+      const skin = skins[index];
+
+      if (skin.name != "null" && skin.images.featured != null) {
+
+        const card = document.createElement('div');
+        card.classList.add('skin-card');
+        card.innerHTML = `
+        <img src="${skin.images.icon}" alt="${skin.description}" data-src="${skin.images.featured}" data-name="${skin.name}">
         <h3>${skin.name}</h3>
       `;
-      container.appendChild(card);
-    });
+        container.appendChild(card);
+      }
+
+    }
+
   })
   .catch(error => console.error(error));
 
-  container.addEventListener('click', event => {
-    const skinCard = event.target.closest('.skin-card');
-    if (skinCard) {
-      const popup = document.createElement('div');
-      popup.classList.add('popup');
-      popup.innerHTML = `
+container.addEventListener('click', event => {
+  const skinCard = event.target.closest('.skin-card');
+  if (skinCard) {
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    popup.innerHTML = `
         <div class="popup-content">
           <img src="${skinCard.querySelector('img').dataset.src}" alt="${skinCard.querySelector('img').alt}">
-          <h3>${skinCard.querySelector('img').dataset.name}</h3>
+          <h1>${skinCard.querySelector('img').dataset.name}</h1>
+          <h3>${skinCard.querySelector('img').alt}</h3>
           <button class="close-btn">Close</button>
         </div>
       `;
-      document.body.appendChild(popup);
-  
-      const closeBtn = popup.querySelector('.close-btn');
-      closeBtn.addEventListener('click', () => {
-        popup.remove();
-      });
-    }
-  });
-  
+    document.body.appendChild(popup);
+
+    const closeBtn = popup.querySelector('.close-btn');
+    closeBtn.addEventListener('click', () => {
+      popup.remove();
+    });
+  }
+});
