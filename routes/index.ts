9194b10fd032381b4e78138e5c4d1efdcf4a7910
Mohamed Bouhdifi <mohamed_bouhdifi@hotmail.com>
2023-05-16 12:35:30 +0200

    User can change details

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,3 @@
-import e from "express";
-
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
@@ -43,12 +41,12 @@ router.post('/Register', (req: any, res: any, next: any) => {
 							email: personInfo.email,
 							username: personInfo.username,
 							password: hashedPassword,
-							ProfilePic: "https://t4.ftcdn.net/jpg/03/32/59/65/360_F_332596535_lAdLhf6KzbW6PWXBWeIFTovTii1drkbT.jpg",	
+							ProfilePic: "https://t4.ftcdn.net/jpg/03/32/59/65/360_F_332596535_lAdLhf6KzbW6PWXBWeIFTovTii1drkbT.jpg",
 							Fortnite: {
 								childFortniteSchema: {
 									Favskins: [Object],
 									BlacklistSkins: [Object],
-																
+
 								},
 							},
 						});
@@ -142,7 +140,7 @@ router.get("/FavouritePage", (req: any, res: any) => {
 			res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
 			res.setHeader('Pragma', 'no-cache');
 			res.setHeader('Expires', '0');
-			return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic  });
+			return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic });
 		}
 	});
 })
@@ -184,7 +182,7 @@ router.post("/FavSkins", (req: any, res: any) => {
 				} else {
 					console.log('User data updated successfully');
 					// render response
-					return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic  });
+					return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic });
 				}
 			});
 		}
@@ -195,51 +193,51 @@ router.post("/UpdateW-L/:skinTitle", (req: any, res: any) => {
 	console.log("UpdateW-L");
 	console.log(req.body);
 	User.findOne({ unique_id: req.session.userId }, function (err: any, data: any) {
-	  if (!data) {
-		res.redirect('/Login');
-	  } else {
-		res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-		res.setHeader('Pragma', 'no-cache');
-		res.setHeader('Expires', '0');
-  
-		const existingSkin = data.Fortnite.Favskins.find((skin: any) => skin.skinTitle === req.params.skinTitle);
-		if (existingSkin) {
-		  console.log('Skin is already Favourited');
-		  existingSkin.wins = req.body.wins;
-		  existingSkin.losses = req.body.losses;
-  
-		  if (existingSkin.losses >= 3) {
-			// Remove the skin from the array if losses are 3 or more
-			data.Fortnite.Favskins = data.Fortnite.Favskins.filter((skin: any) => skin.skinTitle !== req.params.skinTitle);
-  
-			// Add the skin to the blacklist
-			const blacklistedSkin = {
-			  skinTitle: existingSkin.skinTitle,
-			  description: existingSkin.description,
-			  imageProfile: existingSkin.imageProfile,
-			  image: existingSkin.image,
-			  reason: "Excessive losses"
-			};
-			data.Fortnite.BlacklistSkins.push(blacklistedSkin);
-		  }
+		if (!data) {
+			res.redirect('/Login');
 		} else {
-		  console.log('Skin is not Favourited');
+			res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+			res.setHeader('Pragma', 'no-cache');
+			res.setHeader('Expires', '0');
+
+			const existingSkin = data.Fortnite.Favskins.find((skin: any) => skin.skinTitle === req.params.skinTitle);
+			if (existingSkin) {
+				console.log('Skin is already Favourited');
+				existingSkin.wins = req.body.wins;
+				existingSkin.losses = req.body.losses;
+
+				if (existingSkin.losses >= 3) {
+					// Remove the skin from the array if losses are 3 or more
+					data.Fortnite.Favskins = data.Fortnite.Favskins.filter((skin: any) => skin.skinTitle !== req.params.skinTitle);
+
+					// Add the skin to the blacklist
+					const blacklistedSkin = {
+						skinTitle: existingSkin.skinTitle,
+						description: existingSkin.description,
+						imageProfile: existingSkin.imageProfile,
+						image: existingSkin.image,
+						reason: "Excessive losses"
+					};
+					data.Fortnite.BlacklistSkins.push(blacklistedSkin);
+				}
+			} else {
+				console.log('Skin is not Favourited');
+			}
+
+			data.save((err: any) => {
+				if (err) {
+					console.log(err);
+					// handle error
+				} else {
+					console.log('User data updated successfully');
+					// render response
+					return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic });
+				}
+			});
 		}
-  
-		data.save((err: any) => {
-		  if (err) {
-			console.log(err);
-			// handle error
-		  } else {
-			console.log('User data updated successfully');
-			// render response
-			return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic  });
-		  }
-		});
-	  }
 	});
-  });
-  
+});
+
 
 //update notes for favourite skin
 router.post("/UpdateNotes/:Title", (req: any, res: any) => {
@@ -267,7 +265,7 @@ router.post("/UpdateNotes/:Title", (req: any, res: any) => {
 				} else {
 					console.log('User data updated successfully');
 					// render response
-					return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic  });
+					return res.render('FavouritePage', { "name": data.username, "email": data.email, "FavSkins": data.Fortnite.Favskins, "Profile": data.ProfilePic });
 				}
 			});
 		}
@@ -286,7 +284,7 @@ router.get("/BlacklistPage", (req: any, res: any) => {
 			res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
 			res.setHeader('Pragma', 'no-cache');
 			res.setHeader('Expires', '0');
-			return res.render('BlacklistPage', { "name": data.username, "email": data.email, "BlacklistSkins": data.Fortnite.BlacklistSkins, "Profile": data.ProfilePic  });
+			return res.render('BlacklistPage', { "name": data.username, "email": data.email, "BlacklistSkins": data.Fortnite.BlacklistSkins, "Profile": data.ProfilePic });
 		}
 	});
 
@@ -417,11 +415,66 @@ router.get("/Profile", (req: any, res: any) => {
 			res.setHeader('Pragma', 'no-cache');
 			res.setHeader('Expires', '0');
 
-			return res.render('Profile', { "name": data.username, "email": data.email, "Profile": data.ProfilePic });
+			return res.render('Profile', { "name": data.username, "email": data.email, "Profile": data.ProfilePic,message: '' });
 		}
 	});
 
 })
+
+// Add the following route handler after the "Profile" route handler
+router.post('/profile/update', function (req: any, res: any, next: any) {
+	const userId = req.session.userId;
+	const currentPassword = req.body['current-password'];
+	const newPassword = req.body['new-password'];
+	const confirmPassword = req.body['confirm-password'];
+	const username = req.body.username;
+
+	// Find the user by unique_id and validate current password
+	User.findOne({ unique_id: userId }, function (err: any, user: any) {
+		if (err) {
+			return res.send({ "Success": "An error occurred while updating the profile." });
+		}
+
+		if (!user) {
+			return res.send({ "Success": "User not found." });
+		}
+
+		// Compare the current password with the stored password hash
+		hashUtils.comparePassword(currentPassword, user.password, function (err: any, isMatch: any) {
+			if (err) {
+				return res.send({ "Success": "An error occurred while comparing passwords." });
+			}
+
+			if (!isMatch) {
+				return res.send({ "Success": "Current password is incorrect." });
+			}
+
+			// Check if a new password is provided and validate it
+			if (newPassword) {
+				if (newPassword !== confirmPassword) {
+					return res.send({ "Success": "New password and confirm password do not match." });
+				}
+
+				// Hash the new password
+				const hashedNewPassword = hashUtils.hash(newPassword);
+				user.password = hashedNewPassword;
+			}
+
+			// Update the username
+			user.username = username;
+
+			// Save the updated user
+			user.save(function (err: any) {
+				if (err) {
+					return res.send({ "Success": "An error occurred while updating the profile." });
+				}
+				// Update the success message after saving the profile
+				return res.render('Profile', { name: user.username, email: user.email, Profile: user.ProfilePic, message: 'Profile updated successfully!' });
+
+			});
+		});
+	});
+});
 
 router.post("/UpdateProfile/:Title", (req: any, res: any) => {
 	console.log("UpdateProfile");
@@ -457,7 +510,7 @@ router.post("/UpdateProfile/:Title", (req: any, res: any) => {
 
 })
 
-			
+
 
 
 router.get("/PrivacyPolicy", (req: any, res: any) => {
